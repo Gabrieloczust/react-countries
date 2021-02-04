529d7f206619bfdd528e53a81d2cf78dfe28490a
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-02-03 22:50:28 -0300

    create routes

---
+++ src/app/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Countries from '../pages/Countries'
+import Countrie from '../pages/Countrie'
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" exact component={Countries} />
+            <Route path="/study" component={Countrie} />
+        </BrowserRouter>
+    )
+}
+
+export default Routes
